8ac7cc07d441ff8456cb857e1815f08b636f48b5
TheLaw30 <Lawr3nce.lu@gmail.com>
2024-04-10 10:10:09 -0700

    poop

--- app/routes/default.py
+++ app/routes/default.py
@@ -10,10 +10,25 @@ def index():
 def aboutus():
     return render_template('aboutus.html')
 
-@app.route('/start_game')
+@app.route('/z_start_game')
 def startgame():
-    return render_template('start_game.html')
+    return render_template('z_start_game.html')
 
+@app.route('/z_reading')
+def reading():
+    return render_template('z_reading.html')
+
+@app.route('/z_math')
+def math():
+    return render_template('z_math.html')
+
+@app.route('/z_math_option1')
+def math_option1():
+    return render_template('z_math_option1.html')
+
+@app.route('/z_math_option2')
+def math_option2():
+    return render_template('z_math_option2.html')
 # @app.route('/start_game')
 # def startgame():
 #     return render_template('start_game.html')
