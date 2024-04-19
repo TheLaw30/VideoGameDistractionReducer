d2ad683bcc8b53378b51233c6b632379065a7618
TheLaw30 <Lawr3nce.lu@gmail.com>
2024-04-19 10:15:28 -0700

    rough draft for quiz game

--- app/routes/default.py
+++ app/routes/default.py
@@ -26,15 +26,32 @@ def math():
 def math_option1():
     return render_template('z_math_option1.html')
 
-@app.route('/z_math_option2')
-def math_option2():
-    return render_template('z_math_option2.html')
+
 @app.route('/z_math_option1_of_option1')
 def Math_option1_of_option1():
     return render_template('z_math_option1_of_option1.html')    
-@app.route('/z_math_option1_of_option2')
-def Math_option1_of_option2():
-    return render_template('z_math_option1_of_option2.html')  
+
+@app.route('/z_math3')
+def math3():
+    return render_template('z_math3.html')
+@app.route('/z_math4')
+def math4():
+    return render_template('z_math4.html')
+@app.route('/z_reading1')
+def reading1():
+    return render_template('z_reading1.html')
+@app.route('/z_reading2')
+def reading2():
+    return render_template('z_reading2.html')
+@app.route('/z_reading3')
+def reading3():
+    return render_template('z_reading3.html')
+@app.route('/z_reading4')
+def reading4():
+    return render_template('z_reading4.html')
+@app.route('/score')
+def score():
+    return render_template('score.html')
 # @app.route('/start_game')
 # def startgame():
 #     return render_template('start_game.html')
